b0537d547fb21ced6b48775d3e95a56d66fc283a
edidiesky <essienedidiong1000@gmail.com>
2024-09-09 18:38:47 -0700

    new fixes

--- frontend/src/components/single/RoomPaymentTab.jsx
+++ frontend/src/components/single/RoomPaymentTab.jsx
@@ -111,7 +111,7 @@ export default function RoomPaymentTab({ room }) {
   }, [bookingdata]);
 
   return (
-    <div className="w-[100%] lg:sticky top-[10%] flex flex-col gap-8">
+    <div className="w-[100%] lg:sticky top-[10%] hidden lg:flex flex-col gap-8">
       <div className="w-full border rounded-lg py-8 px-3 flex flex-col gap-4 md:w-[380px] bg-[#fff] shadows">
         <h4 className="text-3xl px-6 font-bold">
           ₦{room?.price} <span className="font-normal text-sm">/night</span>
