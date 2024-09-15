113f64415eb425f6e1dc4c2bdbbc0166a7421e4c
edidiesky <essienedidiong1000@gmail.com>
2024-09-14 21:12:11 -0700

    Header fixes

--- frontend/src/screens/dashboard/pages/settings/components/index.jsx
+++ frontend/src/screens/dashboard/pages/settings/components/index.jsx
@@ -7,9 +7,9 @@ const DashboardIndex = () => {
     <div className="w-full">
       <div className="w-full pb-20 flex flex-col gap-8">
         <div className="w-full grid lg:grid-cols-2 lg:items-center gap-4 justify-between">
-          <h3 className="text-3xl lg:text-4xl font-bold font-booking_font4">
+          <h3 className="text-3xl lg:text-5xl font-bold family1">
            Account Settings
-            <span className="block pt-2 text-lg font-normal font-booking_font">
+            <span className="block pt-2 text-lg font-normal">
               Make changes on your profile
             </span>
           </h3>
