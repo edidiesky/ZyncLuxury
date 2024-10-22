b0dea6c99bb638d8df3ff95718c89843f891d252
edidiesky <essienedidiong1000@gmail.com>
2024-10-22 15:18:09 -0700

    Fixed the favourite section

--- frontend/src/components/saved/main/maincontent.jsx
+++ frontend/src/components/saved/main/maincontent.jsx
@@ -22,25 +22,11 @@ const RoomLists = () => {
    gap-8"
     >
       {wishisLoading && <Loader />}
-      <div className="w-[95%] max-w-custom_1 relative mx-auto flex flex-col gap-12">
-        {savedRooms?.length === 0 ? (
-          <div className="w-full flex flex-col gap-4 justify-center items-center">
-            <img src="/no_result.jpg" alt="" className="w-[300px] md:w-[400px]" />
-            <h1 className="text-dark leading-[1.3] text-3xl md:text-3xl text-center family2">
-              You have an empty favourites Rooms
-              <Link
-                to={"/search"}
-                style={{ letterSpacing: "2px" }}
-                className="text-[9px] md:text-xs font-normal pb-1 pt-3 w-fit border-b border-[rgba(0,0,0,.5)] uppercase flex items-center gap-4 regular"
-              >
-                Visit our rooms collections
-              </Link>
-            </h1>
-          </div>
-        ) : (
-          <h3 className="text-4xl md:text-5xl font-bold">
+      <div className="w-[90%] max-w-custom_1 relative mx-auto flex flex-col gap-12">
+        {savedRooms?.length !== 0 && (
+          <h3 className="text-4xl md:text-5xl family2">
             Saved Homes
-            <span className="block pt-3 font-normal text-base md:text-xl">
+            <span className="block pt-3 regular text-base md:text-xl">
               Here is your list of your luxurious saved homes
             </span>
           </h3>
@@ -52,7 +38,23 @@ const RoomLists = () => {
         >
           <div className="w-full">
             {savedRooms?.length === 0 ? (
-              ""
+              <div className="w-full flex flex-col gap-4 justify-center items-center">
+                <img
+                  src="/no_result.jpg"
+                  alt=""
+                  className="w-[300px] md:w-[400px]"
+                />
+                <h1 className="text-dark leading-[1.3] text-3xl md:text-3xl text-center family2">
+                  You have an empty favourites Rooms
+                  <Link
+                    to={"/search"}
+                    style={{ letterSpacing: "2px" }}
+                    className="text-[9px] md:text-xs font-normal pb-1 pt-3 w-fit border-b border-[rgba(0,0,0,.5)] uppercase flex items-center gap-4 regular"
+                  >
+                    Visit our rooms collections
+                  </Link>
+                </h1>
+              </div>
             ) : (
               <div className=" gap-8 w-full grid md:grid-cols-2 lg:grid-cols-3">
                 {savedRooms?.map((apartment, index) => {
