0052cd76a731d37a422855fd014a50f159f9a5dc
edidiesky <essienedidiong1000@gmail.com>
2024-10-23 07:57:22 -0700

    Fixed the vertical scroll timing effect

--- frontend/src/constants/utils/SmoothScroll.jsx
+++ frontend/src/constants/utils/SmoothScroll.jsx
@@ -1,8 +1,24 @@
 import React, { useEffect, useRef } from "react";
-import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import { ReactLenis } from "@studio-freight/react-lenis";
 const SmoothScroll = ({ children }) => {
 
-  return <ReactLenis root>{children}</ReactLenis>;
+  return (
+    <ReactLenis
+      options={{
+        duration: 3,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(3, -10 * t)),
+        smoothWheel: true,
+        smoothTouch: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 1, // Adjusts touch scroll sensitivity (higher for more responsiveness)
+        infinite: false, // Disable infinite scrolling
+        direction: "vertical", // Vertical scrolling direction
+      }}
+      root
+    >
+      {children}
+    </ReactLenis>
+  );
 };
 
 export default SmoothScroll;
