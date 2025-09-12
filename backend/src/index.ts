bb81faaa818cd5b2d7b80b12fff7a9d41fdf3209
edidiesky <essienedidiong1000@gmail.com>
2025-09-12 19:23:17 +0100

    fix(vercel build): add the correct path for the endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,17 +4,21 @@ import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 import cors from "cors";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route";
 import roomRoutes from "./routes/room.route";
 import reservationRoutes from "./routes/reservation.route";
 import dotenv from "dotenv";
+import logger from "./utils/logger";
+import mongoose from "mongoose";
+import { errorHandler, NotFound } from "./middleware/error-handler";
+import { connectMongoDB } from "./utils/connectDB";
 dotenv.config();
 const app = express();
 if (!process.env.WEB_ORIGIN) {
   throw new Error("No WEB_ORIGIN value");
 }
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(helmet());
 app.use(morgan("dev"));
 const apiLimiter = rateLimit({
@@ -43,12 +47,17 @@ app.use("/api/v1/reservation", reservationRoutes);
 app.get("/health", (_req, res) => {
   res.json({ message: "API is running fine!!!", status: "succes" });
 });
+const PORT = process.env.PORT
 
-/** SWAGGER DOCS */
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-// app.get("/api-docs.json", (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   res.send(specs);
-// });
-
-export default app;
+/** ERROR MIDDLEWARE */
+app.use(NotFound);
+app.use(errorHandler);
+app.listen(PORT, async () => {
+  const mongoUrl = process.env.DATABASE_URL;
+  if (!mongoUrl) {
+    throw new Error("MongoDB connection string is not defined.");
+  }
+  try {
+    await connectMongoDB(mongoUrl);
+  } catch (error) {}
+});
