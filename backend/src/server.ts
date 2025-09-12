00966870d7b27642c43733e855747c907cf1be22
edidiesky <essienedidiong1000@gmail.com>
2025-09-12 19:18:14 +0100

    fix(vercel build): add the correct path for the endpoints

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,18 +3,6 @@ import { errorHandler, NotFound } from "./middleware/error-handler";
 import { connectMongoDB } from "./utils/connectDB";
 import logger from "./utils/logger";
 import app from "./app";
-const PORT = process.env.PORT || 3000;
-async function GracefulShutdown() {
-  logger.info("Shutting down gracefully!!");
-  try {
-    await mongoose.connection.close();
-    logger.info("Mongoose has being disconnected!");
-    process.exit(0);
-  } catch (err) {
-    console.error("Error during shutdown:", err);
-    process.exit(1);
-  }
-}
 
 const mongoUrl = process.env.DATABASE_URL;
 if (!mongoUrl) {
